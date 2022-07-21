37277f94513ad46fefa3d5d46aba7009dd91a0c2
Alex Udrea <alexandru.udrea@student.upt.ro>
2022-07-21 19:21:51 +0300

    Styling for register and login

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "./Contact.css"
+import { borderRadius, maxWidth } from '@mui/system';
+import { TextField } from '@mui/material';
+import Background from "./background.jpg"
 
-function Contact() {
-    return (
-        <div>
-            <h1>This is the contact page</h1>
+export default function Contact() {
+  return (
+    <div style={{ 
+        backgroundImage: `url(${Background})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        position: "absolute",
+        backgroundSize: 'cover',
+        width: '100vw',
+        height: '100vh'}}>
+    <Card id="card"sx={{ 
+        maxWidth: 500 ,
+        maxHeight: 1000, 
+        marginLeft: 70, 
+        marginTop: 10,  
+        }}>
+      <CardMedia sx={{borderRadius: 400 / 2}}
+        component="img"
+        image={require('./profile.png')}
+        height="340"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Udrea Alexandru
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          E-mail: alexandruudrea9@gmail.com
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Github profile</Button>
+        <Button size="small">LinkedIn profile</Button>
+      </CardActions>
+      <div id='form'>
+      <h4>Leave an anonimous message here:</h4>
+        <TextField style={{width: 450, marginLeft: 10, marginBottom:5}}></TextField>
         </div>
-    )
+    </Card>
+    </div>
+  );
 }
-
-export default Contact
